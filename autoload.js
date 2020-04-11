ec0d36aa0df059e5a456fb7efabdf6ef1a0b66fc
TaylorLottner <50132994+TaylorLottner@users.noreply.github.com>
2020-04-11 11:50:28 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/taylorlottner/Biu@1.0/";
-const tips_path = "https://cdn.jsdelivr.net/gh/taylorlottner/Biu@1.0/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/taylorlottner/Biu@1.1/";
+const tips_path = "https://cdn.jsdelivr.net/gh/taylorlottner/Biu@1.1/";
 // const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
